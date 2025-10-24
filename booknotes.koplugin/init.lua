e4d50e4dfe7836a7b6df4fd5bdbf23bb43047dfb
0zd3m1r <ozdemirbrhm@gmail.com>
2025-10-24 14:17:03 +0300

    Create init.lua

---
+++ booknotes.koplugin/init.lua
@@ -0,0 +1,8 @@
+-- koreader/plugins/booknotes.koplugin/init.lua
+
+return {
+    name = "BookNotes",
+    description = "A KOReader plugin that parses a 'book_notes.txt' file into browsable sections.",
+    version = "0.1",
+    main = "main",
+}
